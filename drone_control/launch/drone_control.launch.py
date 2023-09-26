1aee488a95e33cb24167148cd2bf994e1dd6a057
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-09-26 12:19:02 -0500

    Working mavsdk files recovered

---
+++ drone_control/launch/drone_control.launch.py
@@ -0,0 +1,24 @@
+"""
+Launch file
+"""
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        # Start the mavsdk_server that connects over serial (UART) to the Pixhawk FC
+        ExecuteProcess(cmd=[['echo \'osprey\' | sudo -S \
+                            ~/.local/lib/python3.10/site-packages/mavsdk/bin/mavsdk_server \
+                            serial:///dev/serial0:921600'
+                            ]],
+                       shell=True
+                    ),
+        # Start the drone_control node
+        Node(package='drone_control',
+             executable='drone_control',
+             output='screen',
+            ),
+    ])
--- mavsdk_ros2/setup.py
+++ mavsdk_ros2/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'drone_control'
+package_name = 'mavsdk_ros2'
 
 setup(
     name=package_name,
@@ -10,18 +10,18 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/drone_control.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/drone_mavsdk.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Marno Nel',
+    maintainer='marno',
     maintainer_email='marthinusnel2023@u.northwestern.edu',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'drone_control = drone_control.drone_control:main'
+        'drone_data = mavsdk_ros2.drone_data:main'
         ],
     },
 )
